90b030e6c387710b3f2c1cbd11310c8fd2b44bf2
nminhngoc <minhngoc622003@gmail.com>
2023-11-24 21:35:17 +0700

    qly

---
+++ frmHoaDon.Designer.cs
@@ -0,0 +1,441 @@
+﻿namespace QuanLyNuocSach
+{
+    partial class frmHoaDon
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            MaHoaDon = new DataGridViewTextBoxColumn();
+            MaCS = new DataGridViewTextBoxColumn();
+            MaDHN = new DataGridViewTextBoxColumn();
+            MaHopDong = new DataGridViewTextBoxColumn();
+            MaKhachHang = new DataGridViewTextBoxColumn();
+            TenHoaDon = new DataGridViewTextBoxColumn();
+            TongTien = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            btnBoqua = new Button();
+            btnThoat = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Margin = new Padding(4, 3, 4, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 3, 4, 3);
+            groupBox1.Size = new Size(1151, 289);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(893, 193);
+            textBox7.Margin = new Padding(4, 3, 4, 3);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(179, 33);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(893, 143);
+            textBox6.Margin = new Padding(4, 3, 4, 3);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(179, 33);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(295, 87);
+            textBox5.Margin = new Padding(4, 3, 4, 3);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(179, 33);
+            textBox5.TabIndex = 12;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(893, 92);
+            textBox4.Margin = new Padding(4, 3, 4, 3);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(179, 33);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(295, 138);
+            textBox3.Margin = new Padding(4, 3, 4, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(179, 33);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(295, 188);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(179, 33);
+            textBox2.TabIndex = 9;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(295, 240);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(179, 33);
+            textBox1.TabIndex = 8;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(671, 196);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(95, 25);
+            label8.TabIndex = 7;
+            label8.Text = "Tổng tiền";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(671, 146);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(122, 25);
+            label7.TabIndex = 6;
+            label7.Text = "Tên hóa đơn";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(671, 95);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(144, 25);
+            label6.TabIndex = 5;
+            label6.Text = "Mã khách hàng";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(78, 248);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(128, 25);
+            label5.TabIndex = 4;
+            label5.Text = "Mã hợp đồng";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(78, 196);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(166, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Mã đồng hồ nước";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(78, 146);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Mã chỉ số";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(78, 95);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Mã hóa đơn";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Times New Roman", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(476, 29);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 36);
+            label1.TabIndex = 0;
+            label1.Text = "HÓA ĐƠN";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(groupBox3);
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(0, 295);
+            groupBox2.Margin = new Padding(4, 3, 4, 3);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(4, 3, 4, 3);
+            groupBox2.Size = new Size(1151, 217);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(0, 214);
+            groupBox3.Margin = new Padding(4, 3, 4, 3);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Padding = new Padding(4, 3, 4, 3);
+            groupBox3.Size = new Size(1151, 119);
+            groupBox3.TabIndex = 1;
+            groupBox3.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { MaHoaDon, MaCS, MaDHN, MaHopDong, MaKhachHang, TenHoaDon, TongTien });
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Margin = new Padding(4, 3, 4, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(1151, 217);
+            dataGridView1.TabIndex = 0;
+            // 
+            // MaHoaDon
+            // 
+            MaHoaDon.HeaderText = "Mã hóa đơn";
+            MaHoaDon.MinimumWidth = 8;
+            MaHoaDon.Name = "MaHoaDon";
+            // 
+            // MaCS
+            // 
+            MaCS.HeaderText = "Mã chỉ số";
+            MaCS.MinimumWidth = 8;
+            MaCS.Name = "MaCS";
+            // 
+            // MaDHN
+            // 
+            MaDHN.HeaderText = "Mã đồng hồ nước";
+            MaDHN.MinimumWidth = 8;
+            MaDHN.Name = "MaDHN";
+            // 
+            // MaHopDong
+            // 
+            MaHopDong.HeaderText = "Mã hợp đồng";
+            MaHopDong.MinimumWidth = 8;
+            MaHopDong.Name = "MaHopDong";
+            // 
+            // MaKhachHang
+            // 
+            MaKhachHang.HeaderText = "Mã khách hàng";
+            MaKhachHang.MinimumWidth = 8;
+            MaKhachHang.Name = "MaKhachHang";
+            // 
+            // TenHoaDon
+            // 
+            TenHoaDon.HeaderText = "Tên hóa đơn";
+            TenHoaDon.MinimumWidth = 8;
+            TenHoaDon.Name = "TenHoaDon";
+            // 
+            // TongTien
+            // 
+            TongTien.HeaderText = "Tổng tiền";
+            TongTien.MinimumWidth = 8;
+            TongTien.Name = "TongTien";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 547);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(181, 547);
+            button2.Margin = new Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 34);
+            button2.TabIndex = 3;
+            button2.Text = "Xóa";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(340, 547);
+            button3.Margin = new Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(134, 34);
+            button3.TabIndex = 4;
+            button3.Text = "Sửa";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(507, 547);
+            button4.Margin = new Padding(4, 3, 4, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(134, 34);
+            button4.TabIndex = 5;
+            button4.Text = "Lưu";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // btnBoqua
+            // 
+            btnBoqua.Location = new Point(820, 547);
+            btnBoqua.Margin = new Padding(4, 3, 4, 3);
+            btnBoqua.Name = "btnBoqua";
+            btnBoqua.Size = new Size(134, 34);
+            btnBoqua.TabIndex = 6;
+            btnBoqua.Text = "Bỏ qua";
+            btnBoqua.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            btnThoat.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnThoat.Location = new Point(985, 547);
+            btnThoat.Margin = new Padding(4, 3, 4, 3);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(134, 34);
+            btnThoat.TabIndex = 7;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += button6_Click;
+            // 
+            // frmHoaDon
+            // 
+            AutoScaleDimensions = new SizeF(12F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
+            ClientSize = new Size(1146, 627);
+            Controls.Add(btnThoat);
+            Controls.Add(btnBoqua);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "frmHoaDon";
+            Text = "Hóa Đơn";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn MaHoaDon;
+        private DataGridViewTextBoxColumn MaCS;
+        private DataGridViewTextBoxColumn MaDHN;
+        private DataGridViewTextBoxColumn MaHopDong;
+        private DataGridViewTextBoxColumn MaKhachHang;
+        private DataGridViewTextBoxColumn TenHoaDon;
+        private DataGridViewTextBoxColumn TongTien;
+        private GroupBox groupBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button btnBoqua;
+        private Button btnThoat;
+    }
+}
---
+++ frmHoaDon.cs
@@ -0,0 +1,56 @@
+﻿namespace QuanLyNuocSach
+{
+    public partial class frmHoaDon : Form
+    {
+        public frmHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ahii");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
